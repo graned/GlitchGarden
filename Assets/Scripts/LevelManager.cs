3918592827b0ba4c59ef5213c70380743a9e4983
eduardoanaya <gran_ed@hotmail.com>
2016-04-15 14:43:21 +0200

    Added splash screen and start screen

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+	public float loadNextNevelTimer; 
+
+	void Start(){
+		Invoke ("loadNextLevel", loadNextNevelTimer);
+	}
+
+	public void LoadLevel(string name){
+		SceneManager.LoadScene (name);
+	}
+		
+	public void QuitRequest(){
+		Application.Quit ();
+	}
+
+	public void loadNextLevel(){
+		SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
+	}
+
+}
