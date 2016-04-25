7ede558506aca27d3189b3156911c8fa44c6ae77
eduardoanaya <gran_ed@hotmail.com>
2016-04-25 10:13:52 +0200

    added option controller

---
+++ Assets/Scripts/OptionsController.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+import UnityEngine.UI;
+
+var volumeSlider : Slider;
+var levelManager: LevelManagerJS;
+
+private var musicManager: MusicManagerJS;
+
+function Start () {
+  musicManager = GameObject.FindObjectOfType("MusicManagerJS");
+  Debug.Log(musicManager);
+}
+
+function Update () {
+
+}
